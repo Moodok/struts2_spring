99ff35e22f8be6be2f9cf34bb90459ce11e56ac2
Yevhenii Melnyk <yevhenii_melnyk@epam.com>
2015-10-20 19:48:42 +0300

    @component annotation deleted

--- src/main/java/ua/nure/melnyk/services/UserService.java
+++ src/main/java/ua/nure/melnyk/services/UserService.java
@@ -1,11 +1,9 @@
 package ua.nure.melnyk.services;
 
-import org.springframework.stereotype.Component;
 import ua.nure.melnyk.entities.User;
 
 import java.util.List;
 
-@Component
 public interface UserService {
 
     User getUserByLogin(String login);
